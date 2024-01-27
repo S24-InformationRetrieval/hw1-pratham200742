a1fee1e955ecf35403a732912fccfdfe3b7baee7
pratham200742 <78619390+pratham200742@users.noreply.github.com>
2024-01-26 21:25:05 -0500

    Part 1 and 2

---
+++ setup_index.py
@@ -0,0 +1,106 @@
+import os
+from os.path import dirname, join
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+import re
+import string
+from elasticsearch import Elasticsearch
+
+current_dir = dirname(__file__)
+
+doc_folder = join(current_dir, 'AP_DATA/ap89_collection')
+sw_path = join(current_dir, 'AP_DATA/stoplist.txt')
+
+textMap = {}
+ps = PorterStemmer()
+sw_list = []
+
+pattern = re.compile(r"<DOC>(.*?)</DOC>", re.DOTALL)
+
+es = Elasticsearch("http://localhost:9200")
+
+def stem_text(text):
+    stemmed = []
+    for w in text.split():
+        if w not in sw_list:
+            stemmed.append(ps.stem(w))
+    
+    stemmed_text = " ".join(stemmed)
+    return stemmed_text
+
+
+def parse_file(file_path):
+    with open(file_path, 'r', encoding = 'ISO-8859-1') as file:
+        docs = pattern.findall(file.read())
+        for content in docs:
+            docNo = re.search(r'<DOCNO>(.*?)</DOCNO>', content, re.DOTALL).group(1).strip()
+            text = re.search(r'<TEXT>(.*?)</TEXT>', content, re.DOTALL).group(1).strip()
+
+            stemmed_text = stem_text(text)
+            textMap[docNo] = stemmed_text
+
+
+with open(sw_path, 'r') as file: 
+    for w in file.readlines():
+        sw_list.append(w.strip())
+
+
+for file_name in os.listdir(doc_folder):
+    if file_name != 'readme':
+        file_path = os.path.join(doc_folder, file_name)
+        parse_file(file_path)
+
+print('Parsing Completed')
+
+
+index_name = "ap89_data0"
+configurations = {
+    "settings": {
+        "number_of_shards": 1,
+        "number_of_replicas": 1,
+        "analysis": {
+            "filter": {
+                "english_stop": {
+                    "type": "stop",
+                    "stopwords_path": "stoplist.txt"
+                }
+            }, 
+            "analyzer": {
+                "stopped": {
+                    "type": "custom",
+                    "tokenizer": "standard",
+                    "filter": [
+                        "lowercase",
+                        "english_stop"
+                    ]
+                }
+            }
+        }
+    },
+    "mappings": {
+        "properties": {
+            "content": {
+                "type": "text",
+                "fielddata": True,
+                "analyzer": "stopped",
+                "index_options": "positions"
+            }
+        }
+    }
+}
+
+es.indices.create(index = index_name, body = configurations)
+
+def add_data(_id, text):
+    es.index(
+        index = index_name,
+        document = {
+            'content': text
+        },
+        id = _id)
+
+for key in textMap:
+    add_data(key, textMap[key])
+
+print("All documents have been added to the index!")
+print(len(textMap))
